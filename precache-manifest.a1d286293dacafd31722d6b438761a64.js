949f49a8ade6ee41cbc0916f2315c3e34d04901e
Jordan Allen <land.of.jordan@gmail.com>
2020-01-22 15:48:53 -0800

    Updates

---
+++ precache-manifest.a1d286293dacafd31722d6b438761a64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a49e072815d45f4bc159cd85c59376c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1055eacb3db507465a50",
+    "url": "/portfolio/static/css/main.7d24dcf2.chunk.css"
+  },
+  {
+    "revision": "20f6d23b6060ca8f1199",
+    "url": "/portfolio/static/js/2.66d449a5.chunk.js"
+  },
+  {
+    "revision": "bc0c83fb5c6c8b946b94c2ec61dd7b90",
+    "url": "/portfolio/static/js/2.66d449a5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1055eacb3db507465a50",
+    "url": "/portfolio/static/js/main.ada2b936.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
